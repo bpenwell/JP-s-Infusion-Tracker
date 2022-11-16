3df79e7c5faa45bb663ec7bcd2dc4755eae247b4
Ben <benpenwell@gmail.com>
2022-11-16 12:31:05 -0800

    Initial Commit

---
+++ Jp's app/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Jp's app
+//
+//  Created by Ben Penwell on 11/16/22.
+//  Copyright © 2022 Ben Penwell. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Jp's appTests/Jp_s_appTests.swift
@@ -0,0 +1,34 @@
+//
+//  Jp_s_appTests.swift
+//  Jp's appTests
+//
+//  Created by Ben Penwell on 11/16/22.
+//  Copyright © 2022 Ben Penwell. All rights reserved.
+//
+
+import XCTest
+@testable import Jp_s_app
+
+class Jp_s_appTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
